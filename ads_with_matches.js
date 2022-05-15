59699dae55bf7cc2ba376e2b931b51d112ecd4bc
lukaslehnert <57840156+lukaslehnert@users.noreply.github.com>
2022-05-15 13:39:27 +0200

    Create ads_with_matches.js

---
+++ ads_with_matches.js
@@ -0,0 +1,39 @@
+
+// Report: Ads nach Anzahl der Matches
+
+async function run() {
+  const matches = db.collection("newMatches").get();                    // get match data
+  const ads = db.collection("newAds").get();                            // get ad data
+  const [matchesQuery, adsQuery] = await Promise.all([matches, ads]);   
+  let matched = false;
+  let myArray = {};
+
+  // ADS
+  for (const adsDoc of adsQuery.docs) {
+
+    const ads = adsDoc.data();
+  
+    var matchesPerAd = _.filter(matchesQuery.docs,
+      function(matchDoc){
+        const match = matchDoc.data();
+        if(match.adReference == ads.id) {
+          console.log("Matched");
+          matched = true;
+          return ads.id;
+        } 
+      }
+    );
+    
+  console.log(matched);
+
+  if(matched == true) {
+    console.log(matchesPerAd);
+  }
+
+  matched = false;
+  myArray = {matchesPerAd};
+
+  //var adData = {"adData":myArray, "matchesPerAd":matchesPerAd}
+  //return adData;
+  }
+}
